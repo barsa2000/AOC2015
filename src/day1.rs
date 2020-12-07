2421783f44ab9bfaf91e51f90158e64b0e8296d3
barsa2000 <barsanti-filippo2@hotmail.it>
2020-12-07 18:46:21 +0100

    cleaned for fmt and clippy

--- src/day1.rs
+++ src/day1.rs
@@ -6,28 +6,33 @@ fn parse_input_day1(input: &str) -> Vec<char> {
     input.chars().collect()
 }
 
-
 #[aoc(day1, part1)]
 fn part1(directions: &[char]) -> i32 {
-    directions.iter().map(|d| match d{
-        '(' => 1,
-        ')' => -1,
-        _ => 0
-    }).sum()
+    directions
+        .iter()
+        .map(|d| match d {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        })
+        .sum()
 }
 
 #[aoc(day1, part2)]
 fn part2(directions: &[char]) -> usize {
-    directions.iter().scan(0_i32, |state, d| {
-        match d {
-            '(' => *state += 1,
-            ')' => *state -= 1,
-            _ => ()
-        };
-        (!state.is_negative()).as_option()
-    }).count() + 1
+    directions
+        .iter()
+        .scan(0_i32, |state, d| {
+            match d {
+                '(' => *state += 1,
+                ')' => *state -= 1,
+                _ => (),
+            };
+            (!state.is_negative()).as_option()
+        })
+        .count()
+        + 1
 }
-
 
 #[cfg(test)]
 mod tests {
